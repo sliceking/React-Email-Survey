c021dcff0b8f0a2959602e4beb2c5ebab763ee32
svwielga4@gmail.com <svwielga4@gmail.com>
2018-01-25 10:31:20 -0800

    added logout function

--- services/passport.js
+++ services/passport.js
@@ -9,7 +9,7 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passpost.deseralizeUser((id, done) => {
+passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
     done(null, user);
   });
