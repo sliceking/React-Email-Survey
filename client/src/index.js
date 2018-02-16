00aaabf48d3d5c13b19a50b090dc53c8d6da84f8
svwielga4@gmail.com <svwielga4@gmail.com>
2018-02-16 13:53:17 -0800

    adding axios, redux-thunk, header, connecting frontend to oauth and some component stubs

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,10 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
+import "materialize-css/dist/css/materialize.min.css";
+import reduxThunk from "redux-thunk";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
