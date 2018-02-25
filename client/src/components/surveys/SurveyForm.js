410a6c715d3f191bb487727f6507242823307868
stan wielga <svwielga4@gmail.com>
2018-02-25 11:46:38 -0800

    adding survey form review component and logic

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
   { label: "Survey Title", name: "title" },
@@ -30,7 +31,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys/" className="red btn-flat white-text">
             Cancel
@@ -46,6 +47,8 @@ class SurveyForm extends Component {
 }
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
